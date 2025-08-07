d5b73899039ce15d882d1c94aa9b45b82dc004de
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-07 10:22:27 -0500

    return value

---
+++ return.js
@@ -0,0 +1,8 @@
+function cube(number){
+    const result=number**3;
+    return result;
+}
+const func=cube(4);
+console.log(func);
+const func2=cube(8);
+console.log(func2);
---
+++ returnOdd.js
@@ -0,0 +1,9 @@
+
+function odd(number){
+    if(number%2!==0){
+        return "It is odd number"
+    }
+    return "It is even number"
+}
+
+console.log(odd(4));
