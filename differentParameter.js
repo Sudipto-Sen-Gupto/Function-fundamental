d4d5fd7552a9f038377eb0775c9a6a14d83a34b4
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-07 10:48:52 -0500

    string in function

---
+++ differentParameter.js
@@ -0,0 +1,14 @@
+
+function charOdd(str){
+       
+    console.log(str);
+    console.log(str.length);
+    if(str.length%2!==0){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+let number=charOdd("Paglahunu");
+console.log(number);
