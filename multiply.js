668747da2257c5442a938f71a128ed43db2db3b9
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-08 00:53:10 -0500

    scrutinize the number is odd or not.

--- multiply.js
+++ multiply.js
@@ -1,5 +1,9 @@
+// task-1
+// Take four parameters. Multiply the four numbers and then return the result
 
 function multiply(a,b,c,d){
-console.log(a*b*c*d);
+    const result=a*b*c*d
+    return result;
 }
-multiply(3,4,5,6);
+const newData=multiply(3,4,5,6);
+console.log(newData);
